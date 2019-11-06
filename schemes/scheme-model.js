5bf18c1d2bf1b88bd8c74eac9a98e2263533a56e
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-06 18:05:20 -0500

    adds findsteps helper

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -10,8 +10,17 @@ function findById(id) {
     .first()
 }
 
+function findSteps(id) {
+    return db.select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+    .from('steps')
+    .join('schemes', 'steps.scheme_id', 'schemes.id')
+    .where('scheme_id', id)
+    .orderBy('step_number')
+}
+
 
 module.exports = {
     find,
     findById,
+    findSteps
 };
